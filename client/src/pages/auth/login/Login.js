c251cbdd6e87436a79a5edc16f986d4ba4595af6
Ekehi <ekehiofficial@gmail.com>
2024-06-19 11:17:03 +0100

    log fix

--- client/src/pages/auth/login/Login.js
+++ client/src/pages/auth/login/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useInitData } from '@tma.js/sdk-react';
 
 const Login = () => {
     const initData = useInitData();
