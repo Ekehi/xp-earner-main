f15a5b43c121e40f69d2d002be9cbf610ad5c285
Ekehi <ekehiofficial@gmail.com>
2024-06-21 18:11:51 +0100

    ss

--- client/src/services/context.js
+++ client/src/services/context.js
@@ -1,27 +1,34 @@
-import { jwtDecode } from 'jwt-decode';
-import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
 
-export const tokenData = () => {
-    const token = Cookies.get('JWT');
-    console.log('Token:', token);
+export const isLogged = () => {
+    const token = sessionStorage.getItem('JWT');
+    console.log('Token retrieved:', token);
 
-    if (token === 'loggedout') return false;
+    if (!token || token === 'loggedout') return false;
 
-    if (!token) return false;
     try {
         const decodedToken = jwtDecode(token);
-        console.log('Decoded Token:', decodedToken);
-
         const now = Date.now() / 1000;
-        console.log('Current time:', now);
-        console.log('Token expiry:', decodedToken.exp);
 
-        if (decodedToken.exp < now) {
-            console.log('Token has expired');
-            return false;
-        }
+        return decodedToken.exp > now;
+    } catch (error) {
+        console.log('Error decoding token:', error);
+        return false;
+    }
+};
 
-        console.log('Token is valid');
+export const tokenData = () => {
+    const token = sessionStorage.getItem('JWT');
+    console.log('Token retrieved:', token);
+
+    if (!token || token === 'loggedout') return false;
+
+    try {
+        const decodedToken = jwtDecode(token);
+        const now = Date.now() / 1000;
+
+        if (decodedToken.exp < now) return false;
+
         return decodedToken;
     } catch (error) {
         console.log('Error decoding token:', error);
