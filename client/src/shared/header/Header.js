0224d6228bd77647bc1346d886972ad6366db844
Ekehi <ekehiofficial@gmail.com>
2024-06-24 17:06:44 +0100

    fix

--- client/src/shared/header/Header.js
+++ client/src/shared/header/Header.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import { useContext } from 'react';
 //import { isLogged } from '../../services/context';
 import { AuthContext } from '../../services/authContext';
 import toast from 'react-hot-toast';
