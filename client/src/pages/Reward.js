1686c4f54c63a7b513603dfdea202d368587359b
Ekehi <ekehiofficial@gmail.com>
2024-08-15 13:00:38 +0100

    loader for reward buttons

--- client/src/pages/Reward.js
+++ client/src/pages/Reward.js
@@ -99,6 +99,23 @@ const Reward = ({ user }) => {
     return `${hours}h ${minutes}m ${secs}s`;
   };
 
+  const Loader = ({ className }) => (
+    <div className={className}>
+      <svg
+        width="13"
+        height="14"
+        viewBox="0 0 13 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4.38798 12.616C3.36313 12.2306 2.46328 11.5721 1.78592 10.7118C1.10856 9.85153 0.679515 8.82231   0.545268 7.73564C0.411022 6.64897 0.576691 5.54628 1.02433 4.54704C1.47197 3.54779 2.1845 2.69009 3.08475   2.06684C3.98499 1.4436 5.03862 1.07858 6.13148 1.01133C7.22435 0.944078 8.31478 1.17716 9.28464    1.68533C10.2545 2.19349 11.0668 2.95736 11.6336 3.89419C12.2004 4.83101 12.5 5.90507 12.5 7"
+          stroke="white"
+        />
+      </svg>
+    </div>
+  )  
+
   return (
     <div className="flex flex-col items-center h-fit w-full mb-5 mt-4 mx-5">
       <div className="w-full rounded-2xl h-fit relative mt-1 border-2 border-slate-900">
@@ -115,8 +132,10 @@ const Reward = ({ user }) => {
   className="text-white font-medium text-xs px-4 rounded-full mt-4 border-solid border-white bg-yellow-500 hover:bg-transparent border-s-2"
   onClick={claimDailyReward}
   disabled={(dailyNextClaim && dailyNextClaim > new Date()) || isLoading}
+  loading={isLoading}
 >
   <span>Daily Reward</span>
+  {!isLoading ? text : <Loader className="spinner" />}
   {dailyNextClaim && dailyNextClaim > new Date() && (
     <span className="block">
       {calculateRemainingTime(dailyNextClaim)}
@@ -127,8 +146,10 @@ const Reward = ({ user }) => {
   className="text-white font-medium text-xs py-2 px-4 rounded-full mt-4 border-solid border-white bg-yellow-500 hover:bg-transparent border-s-2"
   onClick={claim12HourReward}
   disabled={(hour12NextClaim && hour12NextClaim > new Date()) || isLoading}
+  loading={isLoading}
 >
   <span>Mine</span>
+  {!isLoading ? text : <Loader className="spinner" />}
   {hour12NextClaim && hour12NextClaim > new Date() && (
     <span className="block">
       {calculateRemainingTime(hour12NextClaim)}
