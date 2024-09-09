bfa50e97689599132542e3b5e7fcf9c55e60f6c3
Ekehi <160106638+Ekehi@users.noreply.github.com>
2024-09-09 21:47:36 +0100

    gt

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,11 +17,12 @@ module.exports = {
       wider: '.05em',
       widest: '6.4px',
     },
-   /*  extend: {
+    extend: {
       backgroundImage: {
-       'start-bg': "url('/public/images/eks.jpg')",
+       'reg-section': "url('/public/images/8.jpg')",
+       'start-bg': "url('/public/eks.jpg')",
       }
-    } */
+    }
   },
 
   variants: {
