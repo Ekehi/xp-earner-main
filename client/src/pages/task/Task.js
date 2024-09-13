1e91912eaa5e3e774c8f4152bc350ad3ae1beadd
Ekehi <160106638+Ekehi@users.noreply.github.com>
2024-09-13 09:38:40 +0100

    Update Task.js

--- client/src/pages/task/Task.js
+++ client/src/pages/task/Task.js
@@ -152,11 +152,11 @@ const Task = ({ }) => {
 
 
     return (
-        <div className="container flex flex-col content-between h-full w-full z-20 aspect-video  bg-black/90 backdrop-blur-xl shadow-lg overflow-auto">
+        <div className="container relative flex flex-col content-between h-full w-full z-20 aspect-video  bg-black/90 backdrop-blur-xl shadow-lg overflow-auto">
 
 
-            <Card className=' absolute flex flex-col justify-center items-center m-auto bottom-0 z-20 w-dvw h-fit bg-black border-t-2 border-yellow-500 rounded-t-3xl '>
-                <Card.Body className='flex flex-col p-4 items-center w-full'>
+            <Card className=' absolute flex flex-col justify-center items-center m-auto bottom-0 z-20 w-dvw h-full bg-black border-t-2 border-yellow-500 rounded-t-3xl '>
+                <Card.Body className='relative h-full flex flex-col p-4 items-center w-full'>
                     <div className='relative -mt-32 mb-4 p-1 border-aninmation w-fit h-fit rounded-t-full'>
                         <div className='relative bg-black p-2 rounded-t-full'>
                             <div className=' relative  h-[100px] w-[100px] border-[3px] bg-black border-yellow-500 p-2 rounded-t-full shadow-sm-light shadow-yellow-500 '
